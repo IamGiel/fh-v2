989fc1bd2547123960159ce4c0da08732a831058
Server <november14giel@gmail.com>
2018-02-03 08:30:36 -0500

    removed hide/show

--- public/js/login.js
+++ public/js/login.js
@@ -1,19 +1,38 @@
 $(document).ready(function () {
-	// when user clicks add-btn
+    
+    
+    
+    $(function () {
+        $("#yourLoggedin").hide();
+        $(".login-button").on("click", function () {
+            event.preventDefault();
+            $("#nav-mobile, #yourLoggedin ").toggle();
+        });
+    });
+    
     
 
-	$(".login-button").on("click", function (event) {
-        event.preventDefault();
+    
+
+        
+    
+    
+    //button ON CLICK
+	// $(".login-button").on("click", function (event) {
+    //     event.preventDefault();
         
 	  
-    console.log("Logging in...");
-    //hide the log in button
-        $("#nav-mobile").hide();
-        $("#nav-mobile").text("You are in!");
+    //     console.log("Logging in...");
+    //     //hide the log in button
+    //     $("#yourLoggedin").show();
+    //     $("#nav-mobile").hide();
+        
+    //     //redirect to main page
+    //     window.location.href = "#http://localhost:8080/homepage";
+    //     //hiding login page
+    //     logged = true;
+    //     //$("#yourLoggedin").show();
 
-        window.location.href = "#http://localhost:8080/homepage";
-       
-
-	});
+	// });
   });
   
