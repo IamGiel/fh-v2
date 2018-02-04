cd25a3b165d36e8ad15eb8b3e5ed150a1482a13f
Server <november14giel@gmail.com>
2018-02-04 01:27:50 -0500

    port

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ io.on('connection', function(socket){
 // =============================================================
 
 db.sequelize.sync().then(function () {
-  http.createServer(app).listen(app.get('port'), function () {
-    console.log('Express server listening on port ' + app.get('port'));
+  app.listen(process.env.PORT || 3000, function () {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
 });
