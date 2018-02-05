b99e33bb7bfc1993eee56ce169fdb8cc2998f021
Server <november14giel@gmail.com>
2018-02-04 23:59:22 -0500

    login

--- public/js/login.js
+++ public/js/login.js
@@ -1,36 +1,14 @@
-//$(document).ready(function () {
+
     
     
-    
-    //$(".login-button").on("click", function () {
-	//event.preventDefault();
-        //$("#yourLoggedin").hide();
-        //});
-    //});
+    $("#yourLoggedin").hide();
+    $(".login-button").on("click", function () {
+	event.preventDefault();
+        alert("your logged in!");
+        location.reload();
+    });
+
     
     
 
     
-
-        
-    
-    
-    //button ON CLICK
-	// $(".login-button").on("click", function (event) {
-    //     event.preventDefault();
-        
-	  
-    //     console.log("Logging in...");
-    //     //hide the log in button
-    //     $("#yourLoggedin").show();
-    //     $("#nav-mobile").hide();
-        
-    //     //redirect to main page
-    //     window.location.href = "#http://localhost:8080/homepage";
-    //     //hiding login page
-    //     logged = true;
-    //     //$("#yourLoggedin").show();
-
-	// });
-  //});
-  
