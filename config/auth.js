399a11255802706434682188a9dc3409533ce14d
Server <november14giel@gmail.com>
2018-02-05 09:23:33 -0500

    gitignore

---
+++ config/auth.js
@@ -0,0 +1,7 @@
+module.exports = {
+    'googleAuth': {
+        'clientID': 'enter client id here',
+        'clientSecret': 'enter client secret here',
+        'callbackURL': 'http://localhost:8000/google/callback'
+    }
+}
--- public/js/login.js
+++ public/js/login.js
@@ -6,6 +6,7 @@
 	event.preventDefault();
         alert("your logged in!");
         location.reload();
+        
     });
 
     
